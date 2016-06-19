bf615a3d153cd8f8c50f6cdba72c8e97d86d6576
Mattias <mw222rs@student.lnu.se>
2016-06-19 22:53:51 +0200

    choo up and running

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const choo = require('choo')
+const app = choo()
+
+app.model({
+  state: { title: 'Set the title' },
+  reducers: {
+    update: (action, state) => ({ title: action.value })
+  }
+})
+
+const mainView = (params, state, send) => choo.view`
+  <main>
+    <h1>${state.title}</h1>
+    <input
+      type="text"
+      oninput=${(e) => send('update', { value: e.target.value })}>
+  </main>
+`
+
+app.router((route) => [
+  route('/', mainView)
+])
+
+const tree = app.start()
+document.body.appendChild(tree)
