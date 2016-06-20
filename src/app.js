e4504f828464e5e3ce4453c4e9b13be22978352b
Mattias <mw222rs@student.lnu.se>
2016-06-20 23:02:57 +0200

    mucking about

--- src/app.js
+++ src/app.js
@@ -1,25 +1,35 @@
-const choo = require('choo');
-const app = choo();
+const choo = require('choo')
+const sf = require('sheetify')
+
+sf('tachyons')
+
+const app = choo()
 
 app.model({
   state: { title: 'Set the title' },
   reducers: {
-    update: (action, state) => ({ title: action.value }),
-  },
-});
+    update: (action, state) => ({ title: action.value })
+  }
+})
 
 const mainView = (params, state, send) => choo.view`
   <main>
-    <h1>${state.title}</h1>
-    <input
-      type="text"
-      oninput=${(e) => send('update', { value: e.target.value })}>
+    <section class="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
+      <h1 class="center">${state.title}</h1>
+      <input  type="text"
+        oninput=${(e) =>
+          send('update', { value: e.target.value })
+        }
+        onkeyup=${(e) =>
+          e.keyCode === 13 &&
+            send('update', { value: e.target.value.toUpperCase() })}>
+    </section>
   </main>
-`;
+`
 
 app.router((route) => [
-  route('/', mainView),
-]);
+  route('/', mainView)
+])
 
-const tree = app.start();
-document.body.appendChild(tree);
+const tree = app.start()
+document.body.appendChild(tree)
